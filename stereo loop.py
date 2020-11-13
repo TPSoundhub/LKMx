5002e75032cecc92e4f6e03d999ec87e50b95170
TPSoundhub <44570387+TPSoundhub@users.noreply.github.com>
2020-11-13 10:10:32 +0100

    Add files via upload

--- stereo loop.py
+++ stereo loop.py
@@ -2,11 +2,15 @@
 #
 # Loop eksempel med løbende plot af signal  - 
 #
-# Knud - SHD - 14 sept. 2020 - Som ex opsamling efter observation i HH.
+# Knud - SHD - 13 nov. 2020 - Som ex opsamling efter observation i HH.
 #
 # Ex til inspiration når der skal rulles på Modul 2.
 # Der er ticks mellem lydene og sweep er ikke så 'smooth' - det kræver noget andet,hvis det skal ske - så kun som illustration.
 # For at lave en smooth sweep vil man lave een lyd der øges i freq i een tabel og konvertere til en lyd der afspilles.
+#
+# andre forbedringsmuligheder: Tabel med freq den løber igennem for at kunne lave flere 'sjove' kombinationer ??
+# Hvordan får jeg overtoner med i denne? Eller skal det bare være et program for sig selv. Skal evt. fjerne ONLY_ONE_SOUNd
+# - dvs muligheden for at blande flere i denne... Giver nok ikke rigtig noget andet end at det forvirrer.
 #
 import numpy as np
 import pygame
@@ -16,9 +20,9 @@ from scipy.fftpack import fft,fftfreq      # scipy pakken skal med i pakken aht 
 # Globale konstanter
 #
 SAMPLERATE     = 44100
-DURATION       = 0.5       # SKAL være >= 0.2 for at koden nedenfor fungerer mht delay/pauser. Angives i sekunder. Eksperimenteres med stødtoner så sæt evt til 1.0
-MAX_AMP        = 32000
-MAX_FREQ       = 6000      # Max freq til plot i FFT og i loop (while løkken)
+DURATION       = 1.0       # SKAL være >= 0.2 for at koden nedenfor fungerer mht delay/pauser. Angives i sekunder. Eksperimenteres med stødtoner så sæt evt til 1.0
+MAX_AMP        = 15000
+MAX_FREQ       = 3000      # Max freq til plot i FFT og i loop (while løkken)
 TIME_TO_PLOT   = 10        # i ms
 PLOT           = True
 ONLY_ONE_CURVE = True
@@ -26,7 +30,7 @@ ONLY_ONE_FFT   = False
 ONLY_ONE_SOUND = True
 SPEED_OF_SOUND = 343       # m/s i luft ved 20 grader celsius
 DELTA          = 0         # DELTA er forskel på frekvens i højre og venstre. hvis > 0 opleves stødtoner. 
-STEP_IN_SWEEP  = 50        # i Hz - den forskel der er mellem freq i while loop i sweep
+STEP_IN_SWEEP  = 100       # i Hz - den forskel der er mellem freq i while loop i sweep
 #
 # Globale variable
 #
@@ -70,7 +74,7 @@ if PLOT:
 pygame.mixer.init(SAMPLERATE, -16, 2)
 
 time = np.linspace(0,1000*DURATION,DURATION*SAMPLERATE)   # alternativ x-akse med tid i millisekunder (ms) fremfor radianer/vinkelværdier
-
+    
 while freq_left < MAX_FREQ:
     #
     # Laver en tabel for hhv højre og venstre med DURATION*freq antal perioder (omgange i enhedscirklen/bølgelængder)
@@ -78,8 +82,8 @@ while freq_left < MAX_FREQ:
     # Man kan lave sin() funktion på hele tabel på een gang. dvs alle værdier i tabel i et hug!
     # Den sidste linie kombinerer de 2 tabeller til een som kan bruges i make_sound. (copy for at få det lagt ud i række i memory)
     #
-    signal_left  = (np.sin(np.linspace(0,DURATION*freq_left*2*np.pi,DURATION*SAMPLERATE))*amp).astype(np.int16)
-    signal_right = (np.sin(np.linspace(0,DURATION*freq_right*2*np.pi,DURATION*SAMPLERATE))*amp).astype(np.int16)
+    signal_left  = (np.sin(np.linspace(0,DURATION*freq_left*2*np.pi,DURATION*SAMPLERATE,endpoint=False))*amp).astype(np.int16)
+    signal_right = (np.sin(np.linspace(0,DURATION*freq_right*2*np.pi,DURATION*SAMPLERATE,endpoint=False))*amp).astype(np.int16)
     signal = np.array((signal_left,signal_right)).T.copy()
     #
     # Bølge længden = hastighed/frekvens = (m/s divideret med 1/s) = meter (ganges med 100 så det er i cm) og med round(x,2) for at give 2 decimaler
@@ -88,9 +92,9 @@ while freq_left < MAX_FREQ:
     #  - Tilsvarende kan set så være at man skal ekspeirmentere med længden af udsnit man plotter for det kombinerede signal..
     #
     blv = round(SPEED_OF_SOUND*100/freq_left,2)
-    print("Frekvens i venstre: "+str(freq_left)+"Hz. Bølgelængden i venstre: "+str(blv)+" cm")
+    print("Venstre: Freq: "+str(freq_left)+"Hz. Bølgelængden: "+"%.2f" % blv+"cm.","Svingningstid: "+"%.4f" % (1000/freq_left),"ms.")
     blr = round(SPEED_OF_SOUND*100/freq_right,2)
-    print("Frekvens i højre:   "+str(freq_right)+"Hz. Bølgelængden i højre:   "+str(blr)+" cm")
+    print("Højre: Freq:   "+str(freq_right)+"Hz. Bølgelængden: "+"%.2f" % blr+"cm.","Svingningstid: "+"%.4f" % (1000/freq_right),"ms.")
     print()
     #
     # Mixer kan håndtere at afspille 8 lyde samtidigt, men her lader vi den kun spille een ad gangen i et sweep. (Med ONLY_ONE_SOUND = True)
@@ -116,10 +120,10 @@ while freq_left < MAX_FREQ:
     # Aht at få tegnet plot færdigt inden en pause bruges plt.pause hvis vi laver plot ellers pygame delay
     #
     if PLOT: plt.pause(DURATION-0.1)
-    else: pygame.time.delay(200)
+    else: pygame.time.delay(500)
     #
     # Med konstanterne kan man styre om der kun tegnes een kurve for den lyd der spiller eller om de tegnes oveni hinanden.
-    # Nedenfor slettes gamle kurver hvis ONLY_ONE_CURVE er True. FFR får særstatus og kan styres individuelt.
+    # Nedenfor slettes gamle kurver hvis ONLY_ONE_CURVE er True. FFT får særstatus og kan styres individuelt.
     #
     if PLOT and ONLY_ONE_CURVE:
         left_plot.lines.remove(left_curve)          
